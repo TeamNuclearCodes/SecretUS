378f0523f6054bc435890bbcf0356f53387b24ae
1337kid <45532566+1337kid@users.noreply.github.com>
2024-07-27 16:14:57 +0530

    feat: function to encrypt data in AES

--- vault/__init__.py
+++ vault/__init__.py
@@ -1,6 +1,15 @@
 from Crypto.Cipher import AES
+from base64 import b64encode, b64decode
+from db.functions.passvault import addNewPassToPassVault
+from hashlib import sha256
 
-def encryptDataUsingAES(data, key):
+def encryptDataUsingAES(data, password):
+    hash = sha256()
+    hash.update(password)
+    key = hash.digest()
     cipher = AES.new(key, AES.MODE_EAX)
     nonce = cipher.nonce
-    cipher.encrypt(b'hello')
+    ciphertext = cipher.encrypt(data)
+    nonce = b64encode(nonce)
+    ciphertext = b64encode(ciphertext)
+    return ciphertext, nonce
