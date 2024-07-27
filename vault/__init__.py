d78290148d541fcc505ab538b230ec666c9e5339
1337kid <45532566+1337kid@users.noreply.github.com>
2024-07-27 17:54:11 +0530

    feat: function to decrypt encrypted password

--- vault/__init__.py
+++ vault/__init__.py
@@ -2,13 +2,25 @@ from Crypto.Cipher import AES
 from base64 import b64encode, b64decode
 from hashlib import sha256
 
-def encryptDataUsingAES(data, masterPassword):
+def getSHA256sum(data):
     hash = sha256()
-    hash.update(masterPassword)
+    hash.update(data)
     key = hash.digest()
+    return key
+
+def encryptDataUsingAES(data, masterPassword):
+    key = getSHA256sum(masterPassword)
     cipher = AES.new(key, AES.MODE_EAX)
     nonce = cipher.nonce
     ciphertext = cipher.encrypt(data)
     nonce = b64encode(nonce)
     encryptedPassword = b64encode(ciphertext)
     return encryptedPassword, nonce
+    
+def decryptDataUsingAES(data, masterPassword, nonce):
+    data = b64decode(data)
+    nonce = b64decode(nonce)
+    key = getSHA256sum(masterPassword)
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+    password = cipher.decrypt(data)
+    return password
