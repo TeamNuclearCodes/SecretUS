9491a04d1186666847374f29438e5d9c9689d6a5
aaron-6960 <138325645+aaron-6960@users.noreply.github.com>
2024-07-28 04:00:03 +0530

    Sidebar icons added

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../images/Logo.svg";
+import lock from "../images/Lock.svg";
+import ssh from "../images/SSH.svg";
+import pgp from "../images/PGP.svg";
+import settings from "../images/Settings.svg";
 
 export const Sidebar = () => {
   return (
@@ -12,16 +16,28 @@ export const Sidebar = () => {
       <div className="sidebarcontent">
         <ul className="sidebarlist">
           <li>
-            <Link to="/passvault" className="sidebarkeys">PASS VAULT</Link>
+            <img src={lock} alt="Example" className="icon" />
+            <Link to="/passvault" className="sidebarkeys">
+              PASS VAULT
+            </Link>
           </li>
           <li>
-            <Link to="/sshkeys" className="sidebarkeys">SSH KEYS</Link>
+            <img src={ssh} alt="Example" className="icon" />
+            <Link to="/sshkeys" className="sidebarkeys">
+              SSH KEYS
+            </Link>
           </li>
           <li>
-            <Link to="/pgpkeys" className="sidebarkeys">PGP KEYS</Link>
+            <img src={pgp} alt="Example" className="icon" />
+            <Link to="/pgpkeys" className="sidebarkeys">
+              PGP KEYS
+            </Link>
           </li>
           <li>
-            <Link to="settings" className="sidebarkeys">SETTINGS</Link>
+            <img src={settings} alt="Example" className="icon" />
+            <Link to="settings" className="sidebarkeys">
+              SETTINGS
+            </Link>
           </li>
         </ul>
       </div>
