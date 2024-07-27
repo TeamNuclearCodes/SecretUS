b14a31f15b461286824f8bc26e9989b67c1cbcdf
aaron-6960 <138325645+aaron-6960@users.noreply.github.com>
2024-07-27 18:42:15 +0530

    update route setup complete

--- db/functions/passvault.py
+++ db/functions/passvault.py
@@ -13,3 +13,28 @@ def addNewPassToPassVault(password='',username='',service='', description='', no
     )
     db.session.add(newpass)
     db.session.commit()
+
+def updatePassToPassVault(password='',username='',service='', description='', nonce="", id=""):
+    try:
+        # Fetch the record by ID
+        entry = db.session.query(PassVault).filter_by(id=id).one_or_none()
+        
+        if entry is None:
+            print("Entry not found.")
+            return
+
+        # Update the fields if new values are provided
+        entry.password = password
+        entry.description = description
+        entry.service = service
+        entry.nonce = nonce
+        entry.username = username
+        
+
+        # Commit the changes to the database
+        db.session.commit()
+        print("Entry updated successfully.")
+        
+    except Exception as e:
+        db.session.rollback()  # Roll back the transaction on error
+        print(f"An error occurred: {e}")
--- routes/api/passvault.py
+++ routes/api/passvault.py
@@ -2,16 +2,26 @@ from flask import Blueprint, request
 from db.functions.settings import getSettings
 from flask import jsonify
 from vault import encryptDataUsingAES
-from db.functions.passvault import addNewPassToPassVault    
+from db.functions.passvault import addNewPassToPassVault,updatePassToPassVault
 
 bp = Blueprint('passvault', __name__, url_prefix='/api/passvault')
 
 @bp.route('/new', methods=('POST',))
-def status():
+def new():
     formData = request.json
     encryptedPassword, nonce = encryptDataUsingAES(
         data=formData['password'].encode('utf-8'),
         masterPassword=formData['key'].encode('utf-8')
     )
-    addNewPassToPassVault(encryptedPassword, formData["username"], formData["service"], formData["description"])
+    addNewPassToPassVault(encryptedPassword, formData["username"], formData["service"], formData["description"], nonce)
+    return ''
+
+@bp.route('/update', methods=('POST',))
+def update():
+    formData = request.json
+    encryptedPassword, nonce = encryptDataUsingAES(
+        data=formData['password'].encode('utf-8'),
+        masterPassword=formData['key'].encode('utf-8')
+    )
+    updatePassToPassVault(encryptedPassword, formData["username"], formData["service"], formData["description"], nonce, formData["id"])
     return ''
