5c8b3663eeaa96f0cd6c0bad2c94890ead454dbf
aaron-6960 <138325645+aaron-6960@users.noreply.github.com>
2024-07-28 05:54:56 +0530

    PassVault ui done

--- frontend/src/pages/PassVault/PassVault.jsx
+++ frontend/src/pages/PassVault/PassVault.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
-import "./PassVault.css"
-
+import "./PassVault.css";
 
 const PassVault = () => {
   const items = [
-    { id: 1, site: "Google", pass: "google" },
-    { id: 2, site: "Instagram", pass: "insta" },
-    { id: 3, site: "Facebook", pass: "fb" },
+    { id: 1, site: "www.google.com", pass: "google" },
+    { id: 2, site: "www.instagram.com", pass: "insta" },
+    { id: 3, site: "www.facebook.com", pass: "fb" },
   ];
-  const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedItem, setSelectedItem] = useState({id:""});
   const [isPasswordEntered, setIsPasswordEntered] = useState(false);
   const [masterPassword, setMasterPassword] = useState("");
 
@@ -31,32 +30,41 @@ const PassVault = () => {
   };
 
   return (
-    <div>
-      <div className="password-list">
-        <ul>
-          {items.map((item) => (
-            <li
-              key={item.id}
-              onClick={() => handleItemClick(item)}
-              className="passVaultItemList"
-            >
-              {item.site}
-            </li>
-          ))}
-        </ul>
-      </div>
+    <div className="passVaultBody">
+      <ul className="password-list">
+        {items.map((item) => (
+          <li
+            key={item.id}
+            onClick={() => {
+              handleItemClick(item);
+              console.log(item, selectedItem);
+            }}
+            className={
+              item.id === selectedItem.id
+                ? "passVaultItemList active"
+                : "passVaultItemList"
+            }
+          >
+            {item.site}
+          </li>
+        ))}
+      </ul>
+      <div className="vr"></div>
       <div className="item-details">
         {!selectedItem ? (
           <div>Nothing selected</div>
         ) : !isPasswordEntered ? (
-          <form onSubmit={handleMasterPassSubmit}>
+          <form onSubmit={handleMasterPassSubmit} className="item-details-form">
+            <div className="item-details-header">Enter Master Password</div>
             <input
               type="password"
               value={masterPassword}
               onChange={handleMasterPassChange}
               placeholder="Enter master password"
             />
-            <button type="submit">Submit</button>
+            <button type="submit" className="button">
+              Submit
+            </button>
           </form>
         ) : (
           <div>
